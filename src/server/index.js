f5ebf07c0fd2825450a13efbecbd7b019138b87b
mostafaelhabrok <mostafa.elhabrok@gmail.com>
2021-07-21 09:22:50 +0200

    1

--- src/server/index.js
+++ src/server/index.js
@@ -16,8 +16,9 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+const port = process.env.PORT || 80
+app.listen(port, function () {
+    console.log('Example app listening on port '+port)
    })
 
 app.get('/test', function (req, res) {
